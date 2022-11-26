3e05da655c4f9fa7cd9c268bfb73dc218601f15d
Yehor Hubenko <112268293+YHubenko@users.noreply.github.com>
2022-11-26 10:53:16 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Sidebar/>
+            <div>
+                <Header/>
+                <Main/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
