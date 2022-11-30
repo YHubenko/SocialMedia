aae87ea9714e032488bfc556041d200d70d57b6b
Yehor Hubenko <112268293+YHubenko@users.noreply.github.com>
2022-11-30 10:54:14 +0200

    update posts design

--- src/data/state.js
+++ src/data/state.js
@@ -4,12 +4,12 @@ let store = {
     _state: {
         homePage: {
             posts: [
-                {id: 0, username: "User1", message: "first post", likesAmount: 15},
-                {id: 1, username: "User2", message: "second post", likesAmount: 23},
-                {id: 2, username: "User3", message: "third post", likesAmount: 4},
-                {id: 3, username: "User4", message: "fourth post", likesAmount: 5},
-                {id: 4, username: "User5", message: "fifth post", likesAmount: 32},
-                {id: 5, username: "User6", message: "sixth post", likesAmount: 64},
+                {id: 0, username: "User1", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur excepturi iste nesciunt sed, sint tempora?", likesAmount: 15},
+                {id: 1, username: "User2", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur excepturi iste nesciunt sed, sint tempora?", likesAmount: 23},
+                {id: 2, username: "User3", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur excepturi iste nesciunt sed, sint tempora?", likesAmount: 4},
+                {id: 3, username: "User4", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur excepturi iste nesciunt sed, sint tempora?", likesAmount: 5},
+                {id: 4, username: "User5", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur excepturi iste nesciunt sed, sint tempora?", likesAmount: 32},
+                {id: 5, username: "User6", message: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur excepturi iste nesciunt sed, sint tempora?", likesAmount: 64},
             ],
             newPostText: 'new post text',
         },
